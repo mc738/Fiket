fb3a6f1a29a36f3e81d9acad5ac38193da3188ff
Max <me.mclifford.dev>
2020-09-28 22:52:15 +0100

    Init

---
+++ Fiket.Common/Library.fs
@@ -0,0 +1,5 @@
+﻿namespace Fiket.Common
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
---
+++ Fiket.Core/Library.fs
@@ -0,0 +1,5 @@
+﻿namespace Fiket.Core
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
